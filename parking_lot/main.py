3b4c0969e0dd9d34105949b8afdf3444ca68afa1
Michael Ramot <mich.ramot@gmail.com>
2025-02-26 16:27:42 +0200

    added current status of the project.

---
+++ parking_lot/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+import requests
+
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+
+
+@app.route("/", methods=['GET'])
+def index():
+    response = requests.get("https://gisn.tel-aviv.gov.il/GisOpenData/service.asmx/GetLayer?layerCode=970&layerWhere=&xmin=&ymin=&xmax=&ymax=&projection=")
+    data = response.json()
+    items = data.get("features")
+    parking_lot_data = []
+    for item in items:
+        parking_lot_data.append(item.get("attributes"))
+    print(parking_lot_data)
+    return render_template('index.html', parking_lot_data=parking_lot_data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="9000", debug=True)
